5201aa7599a5679a76680abb2c52d99b4a5179f7
Brady Mitchelmore <Bradymitchelmore@gmail.com>
2023-09-25 12:39:44 -0230

    updated q3 to work with any size of data

--- Assignment-1/question3.c
+++ Assignment-1/question3.c
@@ -2,66 +2,73 @@
 #include <stdlib.h>
 #include <math.h>
 
-int main(void) {
-    char *filename = "sensor.csv";
-    int *csv_to_arr(char *filename); // Declare the function
-    int *array = csv_to_arr(filename);
-    void print_stats(int *array); // Declare the function
-    print_stats(array);
-    free(array); // Free the memory allocated for the array
-    return 0; // Add the missing semicolon
-}
-
-// The function will take csv file and return an array of integers
-int *csv_to_arr(char *filename) {
-    // open file and check if it exists
+// Function to read data from a CSV file and return an array of integers
+int *csv_to_arr(char *filename, int *size) {
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
-        printf("Error opening file");
+        printf("Error opening file\n");
         return NULL;
     }
 
-    // create the array to store the values
-    int *array = malloc(5 * sizeof(int)); // Allocate memory for the array
-
-    int index = 0;
+    int *array = NULL; // Initialize the array as NULL
     int num;
+    *size = 0; // Initialize the size to 0
 
-    // read the file and store the values in the array
-    while (fscanf(file, "%d\n", &num) > 0) {
-        array[index] = num;
-        index++;
+    while (fscanf(file, "%d\n", &num) == 1) {
+        (*size)++; // Increment the size for each value
+        array = realloc(array, *size * sizeof(int)); // Dynamically allocate memory
+        if (array == NULL) {
+            printf("Memory allocation failed\n");
+            fclose(file);
+            return NULL;
+        }
+        array[*size - 1] = num; // Store the value in the array
     }
 
     fclose(file);
     return array;
 }
 
-// The function will take an array of integers and print the stats
-void print_stats(int *array) {
-    int count = 0;
+// Function to calculate and print statistics
+void print_stats(int *array, int size) {
+    if (size == 0) {
+        printf("No data to calculate statistics\n");
+        return;
+    }
+
+    int count = size;
     int sum = 0;
     float mean = 0.0f;
     float variance = 0.0f;
     float std_deviation = 0.0f;
-    
-    // calculate the stats
-    for (int i = 0; i < 5; i++) {
-        count++;
+
+    for (int i = 0; i < size; i++) {
         sum += array[i];
     }
     mean = (float)sum / count;
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < size; i++) {
         variance += (array[i] - mean) * (array[i] - mean);
     }
     variance = variance / count;
     std_deviation = sqrt(variance);
 
-    // print the stats
     printf("Count: %d\n", count);
     printf("Sum: %d\n", sum);
     printf("Mean: %f\n", mean);
     printf("Variance: %f\n", variance);
     printf("Standard Deviation: %f\n", std_deviation);
+}
+
+int main(void) {
+    char *filename = "sensor.csv";
+    int size;
+    int *array = csv_to_arr(filename, &size);
+
+    if (array != NULL) {
+        print_stats(array, size);
+        free(array);
+    }
+
+    return 0;
 }
