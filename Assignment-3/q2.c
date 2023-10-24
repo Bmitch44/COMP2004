e5d92abdd037520028d7ebce72eb96cf90024bd5
Brady Mitchelmore <Bradymitchelmore@gmail.com>
2023-10-24 19:12:27 -0230

    completed question 2

--- Assignment-3/q2.c
+++ Assignment-3/q2.c
@@ -35,16 +35,32 @@ void read_input() {
 
 void *calculate_average(void *param) {
     // TODO 1: Implement the logic to calculate average
+    for (int i = 0; i < num_elements; i++) {
+        average += numbers[i];
+    }
+    average = (float)average / num_elements;
     pthread_exit(0);
 }
 
 void *calculate_minimum(void *param) {
     // TODO 2: Implement the logic to calculate minimum
+    minimum = numbers[0];
+    for (int i = 0; i < num_elements; i++) {
+        if (numbers[i] < minimum) {
+            minimum = numbers[i];
+        }
+    }
     pthread_exit(0);
 }
 
 void *calculate_maximum(void *param) {
     // TODO 3: Implement the logic to calculate maximum
+    maximum = numbers[0];
+    for (int i = 0; i < num_elements; i++) {
+        if (numbers[i] > maximum) {
+            maximum = numbers[i];
+        }
+    }
     pthread_exit(0);
 }
 
@@ -53,8 +69,20 @@ int main() {
 
     pthread_t threads[NUM_THREADS];
     // TODO 4: Create the threads and associate them with their respective functions
-
+    for (int i = 0; i < NUM_THREADS; i++) {
+        // create 3 different threads, each executing a different function
+        if (i == 0) {
+            pthread_create(&threads[i], NULL, calculate_average, NULL);
+        } else if (i == 1) {
+            pthread_create(&threads[i], NULL, calculate_minimum, NULL);
+        } else {
+            pthread_create(&threads[i], NULL, calculate_maximum, NULL);
+        }   
+    }
     // TODO 5: Wait for the threads to complete using pthread_join
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
 
     printf("The average value is %d\n", average);
     printf("The minimum value is %d\n", minimum);
